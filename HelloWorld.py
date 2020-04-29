6a126a2ed574433760e6622f71a75e163e7c89cd
elxavi82 <ejavier19@gmail.com>
2020-04-29 15:53:18 -0400

    Testing mathlib and pandas libs

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
+import pandas as pd
+import matplotlib
+from matplotlib import pyplot as plt
 print('Hello World')
+x_values = [0, 1, 2, 3, 4]
+y_values = [0, 1, 4, 9, 16]
+plt.plot(x_values, y_values)
+plt.show()
