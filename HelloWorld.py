d1475cf4bd0cbacdeac90f60200af95f8d0577ef
elxavi82 <ejavier19@gmail.com>
2020-04-29 23:31:29 -0400

    'Testing plot from mathlib'

--- HelloWorld.py
+++ HelloWorld.py
@@ -7,3 +7,12 @@ x_values = [0, 1, 2, 3, 4]
 y_values = [0, 1, 4, 9, 16]
 plt.plot(x_values, y_values)
 plt.show()
+
+x = range(12)
+y = [3000, 3005, 3010, 2900, 2950, 3050, 3000, 3100, 2980, 2980, 2920, 3010]
+plt.plot(x, y)
+plt.axis([0, 12, 2900, 3100])
+plt.xlabel('Time')
+plt.ylabel('Dollars spent on coffee')
+plt.title('My Last Twelve Years of Coffee Drinking')
+plt.show()
