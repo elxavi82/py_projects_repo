57b3d23c4abede54dfcfadfd9e47cdb4d4011c47
elxavi82 <ejavier19@gmail.com>
2020-05-07 20:23:27 -0400

    twitch project

--- twitch_project/twitch_data.py
+++ twitch_project/twitch_data.py
@@ -4,8 +4,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
-df_stream = pd.read_csv('csv-data/stream.csv')
-#df_chat = pd.read_csv('csv-data/chat.csv')
+df_stream = pd.read_csv("csv-data/stream.csv")
+df_chat = pd.read_csv("csv-data/chat.csv")
 
 print(df_stream.head(10))
 print(df_chat.head(10))
